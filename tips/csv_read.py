b2d3f8224ff15e09a051b9cffe9d7a6517b8443f
sky0621 <sky14work@gmail.com>
2017-10-27 00:55:33 +0900

    csv

--- tips/csv_read.py
+++ tips/csv_read.py
@@ -4,3 +4,9 @@ with open('data/dummy.csv') as f:
     reader = csv.reader(f)
     for row in reader:
         print(row)
+
+print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+with open('data/dummy.csv') as f:
+    reader = csv.DictReader(f)
+    print(list(reader))
